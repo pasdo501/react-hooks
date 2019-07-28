e97394b59f9867f33bd61d2b287ed3c2ee9dddf4
pasdo501 <dominikpaschke@hotmail.com>
2019-07-29 11:56:59 +1200

    Project Refactor - Step 1. Theme
    - Starting the refactor of the project to make use of React hooks.
    - First step: Theme context - using useContext
    - Refactored some components along the way to use useState (since they
    were class components and therefore couldn't use hooks and would have
    broken without the extra refactor)
    - Also some minor changes (switched to yarn, added some css, fixed a
    spelling error)

--- react-hooks-course/app/index.js
+++ react-hooks-course/app/index.js
@@ -1,49 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { ThemeProvider } from './contexts/theme'
-import Nav from './components/Nav'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Loading from './components/Loading'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ThemeContext from "./contexts/theme";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/Loading";
 
-const Popular = React.lazy(() => import('./components/Popular'))
-const Battle = React.lazy(() => import('./components/Battle'))
-const Results = React.lazy(() => import('./components/Results'))
+const Popular = React.lazy(() => import("./components/Popular"));
+const Battle = React.lazy(() => import("./components/Battle"));
+const Results = React.lazy(() => import("./components/Results"));
 
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
+function App() {
+  const [theme, setTheme] = React.useState("light");
 
-              <React.Suspense fallback={<Loading />} >
-                <Switch>
-                  <Route exact path='/' component={Popular} />
-                  <Route exact path='/battle' component={Battle} />
-                  <Route path='/battle/results' component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+  const value = React.useMemo(() => ({
+    theme,
+    toggleTheme
+  }), [theme])
+
+  return (
+    <Router>
+      <ThemeContext.Provider value={value}>
+        <div className={theme}>
+          <div className="container">
+            <Nav />
+
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" component={Popular} />
+                <Route exact path="/battle" component={Battle} />
+                <Route path="/battle/results" component={Results} />
+                <Route render={() => <h1>404</h1>} />
+              </Switch>
+            </React.Suspense>
           </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
+        </div>
+      </ThemeContext.Provider>
+    </Router>
+  );
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
