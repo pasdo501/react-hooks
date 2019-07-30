5f97f2625691696174b48855b536e23875cd4f87
pasdo501 <dominikpaschke@hotmail.com>
2019-07-31 09:40:09 +1200

    Curriculum - Refactor index.js
    - Mainly changed the context usage - kept old exports to not break the
    app while refactoring file by file
    - Also switched to yarn

--- react-hooks-course-curriculum/app/contexts/theme.js
+++ react-hooks-course-curriculum/app/contexts/theme.js
@@ -2,5 +2,7 @@ import React from 'react'
 
 const { Consumer, Provider } = React.createContext()
 
+export default Consumer;
+
 export const ThemeConsumer = Consumer
 export const ThemeProvider = Provider
